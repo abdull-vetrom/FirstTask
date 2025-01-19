fdf7b21d4b06adec8ace4e3522cacabf2b4ae00f
root <root@ubuntu.myguest.virtualbox.org>
2025-01-19 19:16:49 +0300

    Using try-catch

---
+++ CreateStudents.php
@@ -0,0 +1,36 @@
+<?php
+try {
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+    require_once 'DatabaseConnection.php';
+
+    $input = file_get_contents('php://input');
+    $requestArray = json_decode($input, true);
+
+    if (!empty($requestArray)) {
+
+        foreach ($requestArray as $key => $value) {
+            $requestArray[$key] = mysqli_real_escape_string($linkDB, $value);
+        }
+
+        $columns = implode(', ', array_keys($requestArray));
+        $values = '"' . implode('", "', array_values($requestArray)) . '"';
+
+        $query = "INSERT INTO students ($columns) VALUES ($values)";
+
+        if (mysqli_query($linkDB, $query)) {
+            echo json_encode(['status' => 'success', 'message' => 'Студент успешно добавлен'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        } else {
+            http_response_code(400);
+            echo json_encode(['error' => 'Ошибка в передаваемых данных' . mysqli_error($linkDB)], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            exit;
+        }
+    } else {
+        http_response_code(400);
+        echo json_encode(['error' => 'Данные не заполнены'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        exit;
+    }
+} catch (Throwable $e) {
+    var_dump($e -> getMessage());
+    die;
+}
--- ReadStudents.php
+++ ReadStudents.php
@@ -1,73 +1,78 @@
 <?php
+try {
+    require_once 'DatabaseConnection.php';
 
-require_once 'DatabaseConnection.php';
+    if (isset($_GET["table"]) and $_GET["table"] != "") {
 
-if (isset($_GET["table"]) and $_GET["table"] != "") {
+        $table = mysqli_real_escape_string($linkDB, $_GET["table"]);
 
-    $table = mysqli_real_escape_string($linkDB, $_GET["table"]);
+        if (isset($_GET["students_id"])) {
 
-    if (isset($_GET["students_id"])) {
+            if($_GET["students_id"] != "") {
 
-        if($_GET["students_id"] != "") {
+                $students_id = intval(mysqli_real_escape_string($linkDB, $_GET["students_id"]));
 
-            $students_id = intval(mysqli_real_escape_string($linkDB, $_GET["students_id"]));
+                $checkStudents_idQuery = "SELECT COUNT(*) as count FROM $table WHERE students_id = $students_id";
+                checkingDataExistence($checkStudents_idQuery, $students_id);
 
-            $checkStudents_idQuery = "SELECT COUNT(*) as count FROM $table WHERE students_id = $students_id";
-            checkingDataExistence($checkStudents_idQuery, $students_id);
+                $query = "SELECT * FROM $table WHERE students_id = $students_id";
+                $result = mysqli_query($linkDB, $query);
 
-            $query = "SELECT * FROM $table WHERE students_id = $students_id";
-            $result = mysqli_query($linkDB, $query);
+                if ($result) {
+
+                    $data = [];
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $data[] = $row;
+                    }
+
+                    echo json_encode(['status' => 'True', 'data' => $data], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(['error' => 'Ошибка выполнения запроса ' . mysqli_error($linkDB)], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+                }
+
+            } else {
+                http_response_code(400);
+                echo json_encode(['error' => 'Параметр students_id не задан'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            }
+        } else {
+            $query = "SELECT * FROM $table";
+            $result = mysqli_query($linkDB,$query);
 
             if ($result) {
-
                 $data = [];
 
                 while ($row = mysqli_fetch_assoc($result)) {
                     $data[] = $row;
                 }
 
-                echo json_encode(['status' => 'True', 'data' => $data], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+                echo json_encode(['status' => true,'data' => $data], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
             } else {
                 http_response_code(400);
                 echo json_encode(['error' => 'Ошибка выполнения запроса ' . mysqli_error($linkDB)], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
             }
-
-        } else {
-            http_response_code(400);
-            echo json_encode(['error' => 'Параметр students_id не задан'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
         }
+
     } else {
-        $query = "SELECT * FROM $table";
-        $result = mysqli_query($linkDB,$query);
+        http_response_code(400);
+        echo json_encode(['error' => 'Параметр table не задан'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    }
 
-        if ($result) {
-            $data = [];
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                $data[] = $row;
-            }
-
-            echo json_encode(['status' => true,'data' => $data], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-        } else {
+    function checkingDataExistence($query, $name) {
+        global $linkDB;
+        $resultCheckQuery = mysqli_query($linkDB, $query);
+        $countRowResultCheckQuery = mysqli_fetch_assoc($resultCheckQuery);
+        if ($countRowResultCheckQuery["count"] == 0) {
             http_response_code(400);
-            echo json_encode(['error' => 'Ошибка выполнения запроса ' . mysqli_error($linkDB)], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            echo json_encode(['error' => "Неправильное значение переменной ($name)"], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            exit;
         }
     }
-
-} else {
-    http_response_code(400);
-    echo json_encode(['error' => 'Параметр table не задан'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+} catch (Throwable $e) {
+    var_dump($e -> getMessage());
+    die;
 }
 
-function checkingDataExistence($query, $name) {
-    global $linkDB;
-    $resultCheckQuery = mysqli_query($linkDB, $query);
-    $countRowResultCheckQuery = mysqli_fetch_assoc($resultCheckQuery);
-    if ($countRowResultCheckQuery["count"] == 0) {
-        http_response_code(400);
-        echo json_encode(['error' => "Неправильное значение переменной ($name)"], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-        exit;
-    }
-}
 
 
