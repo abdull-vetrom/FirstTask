c42b74b7aa79249fcc37080944fc88740d831033
user <user@ubuntu.myguest.virtualbox.org>
2025-02-13 22:50:12 +0300

    refactor(API.php): добавление функции вывода ошибок и удаление лишнего

--- API.php
+++ API.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/bootstrap.php';
+register_shutdown_function(function (){var_dump(error_get_last()); die();});
 
 $entityManager = getEntityManager();
 
@@ -8,9 +9,16 @@ $act = $_GET['act'] ?? null;
 $method = $_GET['method'] ?? null;
 
 $controllerName = 'app\\controllers\\' . ucfirst($act) . 'Controller';
+
 if (!class_exists($controllerName) || !method_exists($controllerName, $method)) {
     printErrorMessage(400, 'Переданные параметры неверные');
 }
+
 $controller = new $controllerName($entityManager);
-$response = $controller->$method($_REQUEST);
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true) ?? [];
+$fullData = $data + $_REQUEST;
+
+$response = $controller->$method($fullData);
 echo json_encode(['status' => 'true', 'data' => $response], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
